6b8cc8926f10d6ebffb2a5a3379ca6c343537f13
ocelotdive <oscar1dlh@hotmail.com>
2020-07-20 08:22:54 +0100

    add: qcard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 
-const QuestionCard = () => <div>Question Card</div>;
+interface Props  {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNum: number;
+  totalQuestions: number;
+}
 
+
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNum, totalQuestions }) =>
+  (
+  
+    <div>
+      <p className="number">
+        Question: {questionNum} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map(answer => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{__html: answer}}></span>
+            </button>
+          </div>
+        ))}
+      </div>
+  </div>
+);
 
 export default QuestionCard;
